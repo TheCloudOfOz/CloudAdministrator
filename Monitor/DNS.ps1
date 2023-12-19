5d2f259e8f4b99fbd7ea37ee326d5c5ce1a7cc1a
Simon Ozturk <simon@stknowledge.com>
2023-12-20 07:11:55 +1100

    Update DNS settings in Settings.json

---
+++ Monitor/DNS.ps1
@@ -0,0 +1,35 @@
+$Settings = Get-Content -Path .\Settings.json | ConvertFrom-Json
+
+foreach ($record in $Settings.DNS.Internal.Records) {
+    
+    if ([String]::IsNullOrEmpty($record.Name)) {
+        $Query = @{
+            Name = $Settings.DNS.Internal.Domain
+            Type = $record.Type
+            Server = $Settings.DNS.Internal.Server
+        }
+        try {
+            $Result = Resolve-DnsName @Query -DnsOnly -ErrorAction Stop
+            $Result | Select-Object -Property Name,Type,IPAddress | Format-Table -AutoSize
+        }
+        catch {
+            Write-Host $Query -ForegroundColor Red
+        }
+        
+    }
+    else {
+        $Query = @{
+            Name = "$($record.Name).$($Settings.DNS.Internal.Domain)"
+            Type = $record.Type
+            Server = $Settings.DNS.Internal.Server
+        }
+        try {
+            $Result = Resolve-DnsName @Query -DnsOnly -ErrorAction Stop
+            $Result | Select-Object -Property Name,Type,IPAddress | Format-Table -AutoSize
+        }
+        catch {
+            Write-Host @Query -ForegroundColor Red
+        }
+    }
+    
+}
